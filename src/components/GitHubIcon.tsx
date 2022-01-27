47016c4f684efd06e708845999cb787fe70e9e5e
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2022-01-27 13:07:32 +0100

    feat: added GitHub & Link icons components

---
+++ src/components/GitHubIcon.tsx
@@ -0,0 +1,30 @@
+import MuiGitHubIcon from '@mui/icons-material/GitHub';
+import type { SvgIconProps } from '@mui/material';
+import { useTheme } from '@mui/material';
+import { ExternalLink } from './ExternalLink';
+
+interface GitHubIconProps extends SvgIconProps {
+   link: string;
+}
+
+export const GitHubIcon = ({ link, sx, ...rest }: GitHubIconProps) => {
+   const theme = useTheme();
+
+   return (
+      <ExternalLink
+         href={link}
+         sx={{
+            pointerEvents: 'auto',
+         }}>
+         <MuiGitHubIcon
+            color="secondary"
+            sx={{
+               transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+               ':hover': { color: theme.palette.primary.main },
+               ...sx,
+            }}
+            {...rest}
+         />
+      </ExternalLink>
+   );
+};
---
+++ src/components/LinkIcon.tsx
@@ -0,0 +1,30 @@
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import type { SvgIconProps } from '@mui/material';
+import { useTheme } from '@mui/material';
+import { ExternalLink } from './ExternalLink';
+
+interface LinkIconProps extends SvgIconProps {
+   link: string;
+}
+
+export const LinkIcon = ({ link, sx, ...rest }: LinkIconProps) => {
+   const theme = useTheme();
+
+   return (
+      <ExternalLink
+         href={link}
+         sx={{
+            pointerEvents: 'auto',
+         }}>
+         <OpenInNewIcon
+            color="secondary"
+            sx={{
+               transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+               ':hover': { color: theme.palette.primary.main },
+               ...sx,
+            }}
+            {...rest}
+         />
+      </ExternalLink>
+   );
+};
